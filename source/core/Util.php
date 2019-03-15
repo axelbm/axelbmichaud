e42a98d76310f0727698c9a7bb4a64991f906ab1
Axel Michaud <axelbmichaud@gmail.com>
2019-03-15 10:00:05 -0400

    Ajout de Util::formatDate

--- source/core/Util.php
+++ source/core/Util.php
@@ -4,26 +4,30 @@ namespace core;
 
 class Util {
 
-    public static function className(string $class) : ?string {
+    static public function className(string $class) : ?string {
         if ($pos = strrpos($class, '\\')) return substr($class, $pos + 1);
         return null;
     }
 
-    public static function objectName(object $obj) : string {
+    static public function objectName(object $obj) : string {
         $class = get_class($obj);
 
         return self::className($class);
     }
 
-    public static function randomKey(?int $length=16) : string {
+    static public function randomKey(?int $length=16) : string {
         return bin2hex(random_bytes($length));
     }
     
-    public static function password_hash(string $password) : string {
+    static public function password_hash(string $password) : string {
         return password_hash($password, PASSWORD_DEFAULT);
     }
 
-    public static function password_verify($password, $hash) : bool {
+    static public function password_verify($password, $hash) : bool {
         return password_verify($password, $hash);
     }
+
+    static public function formatDate(\Datetime $date) : string {
+        return $date->format('l, j F Y H:i');
+    } 
 }

7799bfeeb3dce7223b47d2c3c0efbcf33bfe2cbc
Axel Michaud <axelbmichaud@gmail.com>
2019-03-15 09:56:45 -0400

    Ajout de Blog::getListe et getTags

--- source/app/dao/Blog.php
+++ source/app/dao/Blog.php
@@ -4,6 +4,7 @@ namespace app\dao;
 
 use \core\DAO;
 use \app\modeles;
+use core\Database;
 
 class Blog extends DAO {
     protected $table = "blogs";
@@ -19,6 +20,50 @@ class Blog extends DAO {
         "Categorie" => "categorie:string",
         "Tags" => "tags:array",
 
-        "Auteur" => "AuteurCourriel:Utilisateur:FK:courriel"
+        "Auteur" => "AuteurCourriel:Utilisateur:FK,S:courriel"
     ];
+
+    /**
+     * Retourne un liste de blogs.
+     * 
+     * int "page" - la n em page.
+     * int "npp" - le nombre de blog par page.
+     * string "tags" (tag séparé par des virgule) - des tags de recherche.
+     * string "recherche" - recherche par nom, tags ou par auteur.
+     * string "ordre" (date, nom, vue, commentaire)
+     *
+     * @param array $option 
+     * @return array
+     */
+    public function getListe(array $option=[]) : array {
+        $option["categorie"] = isset($option["categorie"]) ? $option["categorie"] : "blog";
+        
+        return $this->select("WHERE categorie=?", $option["categorie"]);
+    }
+
+    
+    public function getTags(?string $categorie="blog") : array {
+        $stmt = Database::query("SELECT tags FROM blogs WHERE categorie=?", $categorie);
+
+        $resultTags = [];
+        $passedTags = [];
+
+        while ($result = $stmt->fetchColumn()) {
+            $tags = explode(",", $result);
+
+            foreach ($tags as $tag) {
+                $tag = trim($tag);
+
+                if (!isset($passedTags[$tag])) {
+                    $passedTags[$tag] = true;
+                    array_push($resultTags, $tag);
+                }
+            }
+        }
+
+        return $resultTags;
+    }
+
+
+    
 }

0c8f7b71de2556a218a4737dbe8b71d28d9b006e
Axel Michaud <axelbmichaud@gmail.com>
2019-03-15 10:12:43 -0400

    Modification du controleur Database

--- source/app/controleurs/admin/Database.php
+++ source/app/controleurs/admin/Database.php
@@ -2,12 +2,27 @@
 
 namespace app\controleurs\admin;
 
-use app\controleurs\atraits;
+use exceptions\Erreur404;
+use exceptions\PasImplementer;
 
 class Database extends \core\Controleur {
 
     public function action(array $args) : ?\Exception {
+        if ($this->route("connexion")) {
+            return $this->connexion();
+        }
 
+        return new Erreur404();
     }
 
+
+    public function connexion() : ?\Exception {
+        $vue = $this->genererVue("admin/database/connexion");
+			
+        $vue->setDisposition("admin");
+        
+		$vue->afficher();
+
+		return null;
+    }
 }
